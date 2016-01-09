f8f9fb3b56dcc32a7d4992eeb18fd5e2ed3f234e
Pyali <alihamzakocack@gmail.com>
2016-01-09 08:24:36 +0200

    first commit

--- getset/main.cpp
+++ getset/main.cpp
@@ -1,15 +1,70 @@
-//
-//  main.cpp
-//  getset
-//
-//  Created by hamza alı on 9.01.2016.
-//  Copyright © 2016 hamza alı. All rights reserved.
-//
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using System;
+class dortgen
+{
+    private int saat;
+    private int dakika;
+    private int saniye;
+    // Saat değerini kontrol et
+    public int Saat
+    {
+        get
+        {
+            return saat;
+        }
+        set
+        {
+            if (value < 24 && value >= 0)
+                saat = value;
+            else
+                saat = 00;
+        }
+    }
+    // Dakika değerini kontrol et
+    public int Dakika
+    {
+        get
+        {
+            return dakika;
+        }
+        set
+        {
+            if (value < 60 && value >= 00)
+                dakika = value;
+            else
+                dakika = 00;
+        }
+    }
+    // Saniye değerini kontrol et
+    public int Saniye
+    {
+        get
+        {
+            return saniye;
+        }
+        set
+        {
+            if (value < 60 && value >= 0)
+                saniye = value;
+            else
+                saniye = 00;
+        }
+    }
+    public void saatGoruntule()
+    {
+        Console.WriteLine(saat + " : " + dakika + " : " + saniye);
+    }
+    
 }
+class uygulama
+{
+    static void Main()
+    {
+        dortgen zaman = new dortgen();
+        zaman.Saat = 19;
+        zaman.Dakika = 07;
+        zaman.Saniye = 59;
+        zaman.saatGoruntule();
+    }
+}
+
