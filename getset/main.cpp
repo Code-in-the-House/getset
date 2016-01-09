0f0aa1b137dbe2d589920a585fb6e6a28f786ba8
Pyali <alihamzakocack@gmail.com>
2016-01-09 08:21:58 +0200

    Initial Commit

---
+++ getset/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  getset
+//
+//  Created by hamza alı on 9.01.2016.
+//  Copyright © 2016 hamza alı. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
